8055b7cf4c4f4cc0ce3ac999d44e42ef26a8da22
anhnq-cmd <quanganhqb04@gmail.com>
2026-08-20 18:05:55 +0700

    chore: remove Hilt temporarily to avoid Kapt JDK 17 module conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    kotlin("kapt")
-    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -68,10 +66,6 @@ dependencies {
     
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.5")
-    
-    // Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-android-compiler:2.48")
     
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")

b07cc7b51a147855ddefe9e21ecb8a28c5ccbdf4
anhnq-cmd <quanganhqb04@gmail.com>
2026-08-20 17:37:45 +0700

    fix: add missing hilt compiler plugin and dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
     id("com.google.dagger.hilt.android")
 }
 
@@ -70,7 +71,7 @@ dependencies {
     
     // Hilt
     implementation("com.google.dagger:hilt-android:2.48")
-    // kapt("com.google.dagger:hilt-android-compiler:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
     
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
